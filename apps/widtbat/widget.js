1951294816186bc1c02322e5fab8c32d08a7dc66
jacqueline leykam <me@jacqueline.id.au>
2022-07-15 08:14:44 +1000

    stuf

--- apps/widtbat/widget.js
+++ apps/widtbat/widget.js
@@ -4,7 +4,7 @@ Bangle.on('charging', function(charging) {
 });
 WIDGETS["widtbat"] = { area:"tr", width:32, draw: function() {
   const xo = 6, xl = 22, yo = 9, h = 17;
-  g.reset().setColor("#000").drawImage(require("heatshrink").decompress(atob("j0TwIHEv///kD////EfAYPwuEAgPB4EAg/HCgMfzgDBvwOC/IOC84ONDoUcFgc/AYOAHYRDE")), this.x + 1, this.y + 4);
+  g.reset().setColor("#fff").drawImage(require("heatshrink").decompress(atob("j0TwIHEv///kD////EfAYPwuEAgPB4EAg/HCgMfzgDBvwOC/IOC84ONDoUcFgc/AYOAHYRDE")), this.x + 1, this.y + 4);
   g.clearRect(this.x + xo, this.y + yo, this.x + xl, this.y + h);
   var cbc = (Bangle.isCharging()) ? "#f0f" : "#0f0";
   g.setColor(cbc).fillRect(this.x + xo, this.y + yo, this.x + (xl - xo) / 100 * E.getBattery() + xo, this.y + h);
